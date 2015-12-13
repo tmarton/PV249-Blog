08a9e39e19380d274dcbfa5718f4d7907883039a
tmarton <tomas.marton22@gmail.com>
2015-12-13 22:14:00 +0100

    corrected production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'https://whispering-earth-5449.herokuapp.com/'}
+
+
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
